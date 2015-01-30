1c479e2a997acd902348e78f9e375825fbd88c5d
Douglas Hirsh <dhirshjr@gmail.com>
2015-01-30 09:15:21 -0600

    Added more fields. Naive add form is now adding items.

--- pricebook.js
+++ pricebook.js
@@ -5,5 +5,25 @@ if(Meteor.isClient){
     items: function() {
       return Items.find();
     }
+  });
+
+  Template.addItem.events({
+    'submit form': function(e, b){
+      var newItem = {
+        store: $('#itemStore').val(),
+        name: $('#itemName').val(),
+        weight: $('#itemWeight').val(),
+        weightType: $('#itemWeightType').val(),
+        qty: $('#itemQty').val(),
+        qtyType: $('#itemQtyType').val(),
+        price: $('#itemPrice').val()
+      };
+
+      Items.insert(newItem);
+
+      $('#addItemForm').find('input:text').val('');
+
+      return false;
+    }
   });  
 }
