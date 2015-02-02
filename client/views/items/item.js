7f1537c5e074827cd5d93aab4183147e7fbb7337
Douglas Hirsh <dhirshjr@gmail.com>
2015-02-01 23:53:17 -0600

    Inline edit form is working

--- client/views/items/item.js
+++ client/views/items/item.js
@@ -1,6 +1,32 @@
+Template.item.helpers({
+  editing: function(){
+    return Session.equals('editItemId', this._id);
+  }
+});
+
 Template.item.events({
   'click .deleteItem': function(){
     Items.remove(this._id);
+  },
+  'click .editItem': function(){
+    Session.set('editItemId', this._id);
+  },
+  'click .cancelItem': function(){
+    Session.set('editItemId', null);
+  },
+  'click .saveItem': function(){
+    var editItem = {
+      store: $("#editItemStore").val(),
+      name: $("#editItemName").val(),
+      weight: $("#editItemWeight").val(),
+      weightType: $("#editItemWeightType").val(),
+      qty: $("#editItemQty").val(),
+      qtyType: $("#editItemQtyType").val(),
+      price: $("#editItemPrice").val()
+    }
+
+    Items.update(Session.get('editItemId'), editItem);
+    Session.set('editItemId', null);
   }
 });
 
