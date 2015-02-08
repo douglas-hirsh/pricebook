791d6cc27d4b8956ca01edb7e29fa1152221c7f7
Douglas Hirsh <dhirshjr@gmail.com>
2015-02-07 23:52:40 -0600

    moved add item into the table and finished styling

--- client/views/items/additem.js
+++ client/views/items/additem.js
@@ -1,24 +1,37 @@
+var addItem = function(){
+  var newItem = {
+    store: $('#itemStore').val(),
+    name: $('#itemName').val(),
+    weight: $('#itemWeight').val(),
+    weightType: $('#itemWeightType').val(),
+    qty: $('#itemQty').val(),
+    qtyType: $('#itemQtyType').val(),
+    price: $('#itemPrice').val()
+  };
+  
+  Items.insert(newItem, {validationContext: 'insertForm'}, function(error, result) {
+    if(!error){
+      $('#addItem').find('input:text').val('');
+      $('#itemStore').focus();
+    }
+  });
+}
+
 Template.addItem.events({
-  'submit form': function(e, b){
-    var newItem = {
-      store: $('#itemStore').val(),
-      name: $('#itemName').val(),
-      weight: $('#itemWeight').val(),
-      weightType: $('#itemWeightType').val(),
-      qty: $('#itemQty').val(),
-      qtyType: $('#itemQtyType').val(),
-      price: $('#itemPrice').val()
-    };
-    
-    Items.insert(newItem, {validationContext: 'insertForm'}, function(error, result) {
-      if(!error){
-        $('#addItemForm').find('input:text').val('');
-        $('#itemStore').focus();
-      }
-    });
+  'click button': function(e, b){
     
     return false;
+  },
+  'keypress input': function(e){
+    if(e.keyCode === 13){
+      addItem();
+    }
+    else if(e.keyCode === 27){
+      $('#addItem').find('input:text').val('');
+      $('#itemStore').focus();
+    }
   }
+
   
 });
 
--- client/views/items/item.js
+++ client/views/items/item.js
@@ -26,10 +26,13 @@ Template.item.events({
     Items.remove(this._id);
   },
   'click .editItem': function(){
+    Items.simpleSchema().namedContext('updateForm').resetValidation();
+    Items.simpleSchema().namedContext('insertForm').resetValidation();
     Session.set('editItemId', this._id);
   },
   'click .cancelItem': function(){
     Items.simpleSchema().namedContext('updateForm').resetValidation();
+    Items.simpleSchema().namedContext('insertForm').resetValidation();
     Session.set('editItemId', null);
   },
   'click .saveItem': function(){
