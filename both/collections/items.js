9db7bd7927f704763849f6cbc4671cb588083330
Douglas Hirsh <dhirshjr@gmail.com>
2015-02-19 20:41:46 -0600

    Removed insecure and added allow rules. Cannot save right now.

--- both/collections/items.js
+++ both/collections/items.js
@@ -1,5 +1,21 @@
 Items = new Mongo.Collection('items');
 
+Items.allow({
+  insert: function (userId, doc) {
+    // the user must be logged in, and the document must be owned by the user
+    return (userId && doc.owner === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    // can only change your own documents
+    return doc.owner === userId;
+  },
+  remove: function (userId, doc) {
+    // can only remove your own documents
+    return doc.owner === userId;
+  },
+  fetch: ['owner']
+});
+
 Items.attachSchema(new SimpleSchema({
   store: {
     type: String,
