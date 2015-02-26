933be1ddae6206ba8b50a1c2c9795e5794af3514
Douglas Hirsh <dhirshjr@gmail.com>
2015-02-26 12:55:10 -0600

    Can search on all columns except number type columns.

--- client/views/items/itemsearch.js
+++ client/views/items/itemsearch.js
@@ -1,5 +1,14 @@
 Template.itemsearch.events({
   'keyup .searchInput': _.throttle(function(e, t) {
-    Session.set('searchQuery', {name: t.$('.searchInput').val()});
+    var searchQuery = Session.get('searchQuery') || {};
+    searchQuery[this.columnName] = t.$('.searchInput').val();
+    Session.set('searchQuery', searchQuery);
   },500)
 });
+
+Template.itemsearch.rendered=function(){
+  var searchQuery = Session.get('searchQuery') || {};
+  var searchColumnValue = searchQuery[this.data.columnName] || '';
+
+  this.$('.searchInput').val(searchColumnValue);
+}
--- server/publications/items/item.js
+++ server/publications/items/item.js
@@ -1,8 +1,10 @@
 Meteor.publish("allItems", function (searchQuery) {
   var mongoQuery = {};
-  _.each(_.keys(searchQuery), function(key){
-    mongoQuery[key] = {$regex: searchQuery[key], $options: 'i'};
-  });
+  if(searchQuery){
+    _.each(_.keys(searchQuery), function(key){
+      mongoQuery[key] = {$regex: searchQuery[key], $options: 'i'};
+    });
+  }
 
   return Items.find(mongoQuery,{limit:10});
 });
